de1ab7f0f44f03ee8cefb7ad2497f317e782890b
Raphael Brandão <raphael.b.souza@hotmail.com>
2018-10-28 11:31:56 -0300

    Log arrays

--- src/utils/Log.java
+++ src/utils/Log.java
@@ -1,10 +1,23 @@
 package utils;
 
+import java.lang.reflect.Array;
+
 public abstract class Log {
 	
 	public static void run(IExecuter executner) {
 		long t = System.currentTimeMillis();
-		System.out.println("Resposta: " + executner.execute());
+		Object execution = executner.execute();
+		if (execution.getClass().isArray()) {
+			System.out.println("Resposta:");
+			for(int i=0; i < Array.getLength(execution); i++){
+				if (i > 0)
+					System.out.print(", ");
+		        System.out.print(Array.get(execution, i));
+		    }
+			System.out.println();
+		} else {			
+			System.out.println("Resposta: " + execution);
+		}
         System.out.println("Tempo: " + (System.currentTimeMillis() - t) + "ms");
 	}
 
