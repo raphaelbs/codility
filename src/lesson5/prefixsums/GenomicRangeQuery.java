4cac8aab8346fef5effa45a00e5efda5026eb021
Raphael Brandão <raphael.b.souza@hotmail.com>
2018-11-08 00:35:50 -0200

    apply bst in GenomicRangeQuery

--- src/lesson5/prefixsums/GenomicRangeQuery.java
+++ src/lesson5/prefixsums/GenomicRangeQuery.java
@@ -41,11 +41,12 @@ public class GenomicRangeQuery {
 
 class Map {
 	private final ArrayList<Integer> AMap, CMap, GMap, TMap;
+	private final TreeManager ATree, CTree, GTree, TTree;
 	private final String S;
 	private final int sLen;
 	private final int[] translatedArr;
 	
-	public Map(String S) {
+	public Map(final String S) {
 		this.S = S;
 		sLen = S.length();
 		translatedArr = new int[sLen];
@@ -53,10 +54,14 @@ class Map {
 		CMap = new ArrayList<Integer>(sLen);
 		GMap = new ArrayList<Integer>(sLen);
 		TMap = new ArrayList<Integer>(sLen);
-		split();
+		populateMaps();
+		ATree = new TreeManager(AMap, translatedArr);
+		CTree = new TreeManager(CMap, translatedArr);
+		GTree = new TreeManager(GMap, translatedArr);
+		TTree = new TreeManager(TMap, translatedArr);
 	}
 	
-	private void split() {
+	private void populateMaps() {
 		it : for (int i=0; i<sLen; i++) {
 			switch (S.charAt(i)) {
 			case 'A':
@@ -83,22 +88,63 @@ class Map {
 		TMap.trimToSize();
 	}
 
-	public int getMinimunAt(int P, int Q) {
+	public int getMinimunAt(final int P, final int Q) {
 		if (Q - P == 0) return translatedArr[P];
-		else if (Q - P == 1) return Math.min(translatedArr[P], translatedArr[Q]);
-		
-		return 1;
+		if (Q - P == 1) return Math.min(translatedArr[P], translatedArr[Q]);
+		if (ATree.containAnyBetween(P, Q)) return 1;
+		if (CTree.containAnyBetween(P, Q)) return 2;
+		if (GTree.containAnyBetween(P, Q)) return 3;
+		if (TTree.containAnyBetween(P, Q)) return 4;
+		return -1;
 	}
 }
 
-class Tree {
-	private Tree root, cRight, cLeft;
-	public Tree() {
-		
-	}
+class TreeManager { 
+    private Node root;
+    private final int[] translatedArray;
+    
+    public TreeManager(final ArrayList<Integer> list, final int[] translatedArray) {
+    	root = listToBST(list, 0, list.size() - 1);
+    	this.translatedArray = translatedArray;
+    }
+    
+    private Node listToBST(final ArrayList<Integer> list, final int start, final int end) {
+        if (start > end) { 
+            return null; 
+        }
+        
+        int mid = (start + end) / 2; 
+        Node node = new Node(list.get(mid)); 
+        node.left = listToBST(list, start, mid - 1);
+        node.right = listToBST(list, mid + 1, end); 
+          
+        return node; 
+    }
+
+    public boolean containAnyBetween(final int P, final int Q) {
+    	return containAny(root, P, Q);
+    }
+    
+    private boolean containAny(final Node currentNode, final int P, final int Q) {
+    	if (currentNode == null) return false;
+    	if (currentNode.data == translatedArray[P] || currentNode.data == translatedArray[Q]) return true;
+    	if (currentNode.data > translatedArray[P] && currentNode.data < translatedArray[Q]) return true;
+    	if (currentNode.data > translatedArray[Q]) return containAny(currentNode.right, P, Q);
+    	if (currentNode.data < translatedArray[P]) return containAny(currentNode.left, P, Q);
+    	return false;
+    }
 }
 
-
+class Node { 
+    
+    int data; 
+    Node left, right; 
+      
+    Node(final int d) { 
+        data = d; 
+        left = right = null; 
+    } 
+} 
 
 
 
