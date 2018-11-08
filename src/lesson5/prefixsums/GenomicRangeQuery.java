6b545ee22ccf820e94f495dea90aff9f9e8cc396
Raphael Brandão <raphael.b.souza@hotmail.com>
2018-11-08 19:56:10 -0200

    add GenomicRangeQuery

--- src/lesson5/prefixsums/GenomicRangeQuery.java
+++ src/lesson5/prefixsums/GenomicRangeQuery.java
@@ -11,9 +11,9 @@ public class GenomicRangeQuery {
 		Log.run(new IExecuter() {
 			@Override
 			public Object execute() {
-				return solution("CAGCCTA",
-						new int[] { 2, 5, 0 },
-						new int[] { 4, 5, 6 });
+				return solution("ATTAATTAATTAATATAT",
+						new int[] { 0, 2, 4, 6, 8 },
+						new int[] { 1, 3, 5, 7, 9 });
 			}
 		});
 		Log.run(new IExecuter() {
@@ -120,8 +120,8 @@ class TreeManager {
     	if (currentNode == null) return false;
     	if (currentNode.data == P || currentNode.data == Q) return true;
     	if (currentNode.data > P && currentNode.data < Q) return true;
-    	if (currentNode.data > Q) return containAny(currentNode.right, P, Q);
-    	if (currentNode.data < P) return containAny(currentNode.left, P, Q);
+    	if (currentNode.data > Q) return containAny(currentNode.left, P, Q);
+    	if (currentNode.data < P) return containAny(currentNode.right, P, Q);
     	return false;
     }
 }
